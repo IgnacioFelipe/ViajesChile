6b8852fa9b7256ea89937da5ddba150bd3d43744
IgnacioFelipe <ignacio.encina.rojas@gmail.com>
2022-01-05 19:48:55 -0300

    agregar animacion

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function()){
+
+    $("a").click(function(){
+        var gato= this.hash
+
+        $("html, body").animate(
+            {
+                scrollTop:$(gato).offset.top
+            },
+            3000
+        )
+    })
+}
